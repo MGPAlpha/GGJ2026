12677e1cab1c1b4b8b6f3b79561434f21e3772ff
gZation <ryanabusa8134@gmail.com>
2026-02-01 01:28:52 -0500

    Imported Basin, and Mave Adjustments

--- scripts/boardtile.gd
+++ scripts/boardtile.gd
@@ -2,25 +2,28 @@ class_name BoardTile extends Node3D
 
 @export var default_color: Color = Color.WHITE
 
-@export var basic_mesh: MeshInstance3D
-@export var source_mesh: MeshInstance3D
+@export var basic_mesh: Node3D
+@export var basic_canvas_mesh: MeshInstance3D
+@export var source_mesh: Node3D
+@export var source_basin_mesh: MeshInstance3D
 @export var clean_mesh: MeshInstance3D
 @export var inert_mesh: MeshInstance3D
 
 var basic_material: StandardMaterial3D
-var source_material: StandardMaterial3D
+var source_material: ShaderMaterial
 var clean_material: StandardMaterial3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 	basic_material = StandardMaterial3D.new()
-	basic_mesh.material_override = basic_material
+	basic_canvas_mesh.material_override = basic_material
 	basic_material.albedo_color = default_color
 	basic_mesh.visible = false
-	source_material = StandardMaterial3D.new()
-	source_mesh.material_override = source_material
-	source_material.albedo_color = default_color
+	source_material = source_basin_mesh.material_override.duplicate()
+	source_basin_mesh.material_override = source_material
+	source_material.set_shader_parameter("PaintColor", default_color)
+	source_material.set_shader_parameter("RandomID", randf())
 	source_mesh.visible = false
 	clean_material = StandardMaterial3D.new()
 	clean_mesh.material_override = clean_material
@@ -34,7 +37,7 @@ func _process(delta: float) -> void:
 
 func set_color(color: Color):
 	basic_material.albedo_color = color
-	source_material.albedo_color = color
+	source_material.set_shader_parameter("PaintColor", color)
 	
 func set_mode(mode: BoardTileData.TileMode):
 	print(mode)
